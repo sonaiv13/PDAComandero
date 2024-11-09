0015fd26b23b2d75a1d1c6572de6c314e4b6df28
BlackFrisian13 <sonaivanyan13@gmail.com>
2024-11-09 20:01:15 +0100

    Cambios en Dialogo Mesas.

--- app/src/main/java/com/example/pdacomandero/ui/dialogs/DialogoMesas.kt
+++ app/src/main/java/com/example/pdacomandero/ui/dialogs/DialogoMesas.kt
@@ -35,12 +35,14 @@ class DialogoMesas : DialogFragment() {
     private lateinit var numMesa: TextView
     private lateinit var recyclerMesas: RecyclerView
     private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
 
     private var mesa: Mesa? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         this.contexto = context
+        auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance("https://pdacomandero-default-rtdb.europe-west1.firebasedatabase.app/")
         listaMesas = ArrayList()
         mesasAdapter = MesasAdapter(listaMesas, context)
@@ -62,44 +64,43 @@ class DialogoMesas : DialogFragment() {
     }
 
     private fun rellenarRecyclerMesa() {
+        val userId = auth.currentUser?.uid
+        if(userId != null) {
+            val databaseRef = database.getReference("usuarios").child(userId).child("mesas")
+            databaseRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    listaMesas.clear()
+                    Log.d("Debug", "Snapshot exists: ${snapshot.exists()}")
 
-        val userId = FirebaseAuth.getInstance().currentUser?.uid
-        if(userId == null) {
-            Snackbar.make(binding.root, "Error: Usuario no autenticado", Snackbar.LENGTH_SHORT).show()
-            return
-        }
+                    if(snapshot.exists()){
+                        snapshot.children.forEach{ mesaSnapshot ->
+                            Log.d("Debug", "Mesa snapshot: $mesaSnapshot")
 
-        val databaseRef = database.getReference("usuarios").child(userId).child("mesas")
-        databaseRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                listaMesas.clear()
-                Log.d("Debug", "Snapshot exists: ${snapshot.exists()}")
-
-                if(snapshot.exists()){
-                    snapshot.children.forEach{ mesaSnapshot ->
-                        Log.d("Debug", "Mesa snapshot: $mesaSnapshot")
-
-                        val idMesa = mesaSnapshot.child("id").getValue(Int::class.java)
-                        val numMesa = mesaSnapshot.child("numero").getValue(Int::class.java)
-                        if(idMesa != null && numMesa != null){
-                            listaMesas.add(Mesa(idMesa, numMesa))
-                        } else {
-                            Log.d("Debug", "Error: mesa data is null or missing")
+                            val idMesa = mesaSnapshot.child("id").getValue(Int::class.java)
+                            val numMesa = mesaSnapshot.child("numero").getValue(Int::class.java)
+                            val disponible = mesaSnapshot.child("disponible").getValue(Boolean::class.java)
+                            val pedidos = mesaSnapshot.child("pedidos").getValue(ArrayList::class.java) as? ArrayList<Pedido> ?: mutableListOf()
+                            if(idMesa != null && numMesa != null && disponible != null){
+                                listaMesas.add(Mesa(idMesa, numMesa, disponible, pedidos = pedidos))
+                            } else {
+                                Log.d("Debug", "Error: mesa data is null or missing")
+                            }
                         }
+                        mesasAdapter.notifyDataSetChanged()
+                    } else {
+                        Snackbar.make(binding.root, "No hay mesas disponibles", Snackbar.LENGTH_SHORT).show()
+                        Log.d("Debug", "Snapshot no contiene mesas.")
                     }
-                    mesasAdapter.notifyDataSetChanged()
-                } else {
-                    Snackbar.make(binding.root, "No hay mesas disponibles", Snackbar.LENGTH_SHORT).show()
-                    Log.d("Debug", "Snapshot no contiene mesas.")
+
                 }
 
-            }
+                override fun onCancelled(error: DatabaseError) {
+                    Snackbar.make(binding.root, "Algo ha fallado con la conexión a internet", Snackbar.LENGTH_SHORT).show()
+                }
 
-            override fun onCancelled(error: DatabaseError) {
-                Snackbar.make(binding.root, "Algo ha fallado con la conexión a internet", Snackbar.LENGTH_SHORT).show()
-            }
+            })
+        }
 
-        })
 
     }
 }
